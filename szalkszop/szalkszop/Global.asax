9c1f34716c1b1c00d0793a024cb21861f04b1926
Konrad <konradszalek@gmail.com>
2017-07-02 13:48:21 +0200

    intial commit 2

---
+++ szalkszop/szalkszop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="szalkszop.MvcApplication" Language="C#" %>
---
+++ szalkszop/szalkszop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(szalkszop.Startup))]
+namespace szalkszop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
