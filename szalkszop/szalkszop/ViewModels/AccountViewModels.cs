c6f8ed35060d5c5cd18be05e3f85e1e0f53d49bd
Konrad <konradszalek@gmail.com>
2018-12-07 12:00:02 +0100

    gitignore updated

--- szalkszop/szalkszop/ViewModels/AccountViewModels.cs
+++ szalkszop/szalkszop/ViewModels/AccountViewModels.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace szalkszop.Core.Models
+namespace szalkszop.ViewModels
 {
     public class ExternalLoginConfirmationViewModel
     {
--- szalkszop/szalkszop/ViewModels/ManageViewModels.cs
+++ szalkszop/szalkszop/ViewModels/ManageViewModels.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
-namespace szalkszop.Core.Models
+namespace szalkszop.ViewModels
 {
     public class IndexViewModel
     {
